a59752f02c3b7dc59dc5c8de631a547972236a9f
Hannah Boeckers <hannah.boeckers@web.de>
2023-04-09 19:18:58 +0200

    fix bugs

--- scripts/display/recipe_display.gd
+++ scripts/display/recipe_display.gd
@@ -22,9 +22,9 @@ func init(recipe_data : RecipeData) -> void:
 	## initialize ingredient list
 	for ingredient in recipe_data.ingredients:
 		var display = ingredient_display_scene.instantiate()
-		display.init(ingredient.base_quantity,ingredient.unit, ingredient.title)
 		ingredient_list_container.add_child(display)
-		display.get_quantity_text_field.text_submitted.connect(calc_quantity.bind(display.get_index()))
+		display.init(ingredient.base_quantity,ingredient.unit, ingredient.ingredient_name)
+		display.get_quantity_text_field().text_submitted.connect(calc_quantity.bind(display.get_index()))
 
 
 ## calculate new quantity for all ingredients based on their base ratios when user enters
@@ -33,7 +33,7 @@ func calc_quantity(new_quantity, ingredient_index) -> void:
 	new_quantity = float(new_quantity)
 	var factor = new_quantity/recipe_data.ingredients[ingredient_index].base_quantity
 	for i in range(0, ingredient_list_container.get_child_count()):
-		ingredient_list_container.get_child(i).set_quantity(recipe_data.ingredients[i].base_quantityfactor)
+		ingredient_list_container.get_child(i).set_quantity(recipe_data.ingredients[i].base_quantity * factor)
 
 
 ## return to recipe list when return button is pressed
