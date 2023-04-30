245515d5c8e2e6999c97b69b43b1707a63540907
Hannah Boeckers <hannah.boeckers@web.de>
2023-04-30 15:58:59 +0200

    export recipes

--- scripts/display/recipe_book_display.gd
+++ scripts/display/recipe_book_display.gd
@@ -4,6 +4,8 @@ extends Control
 
 ## UI scene for recipe preview
 @onready var recipe_preview_scene : PackedScene = preload("res://scenes/recipe_preview.tscn")
+@onready var file_dialog: FileDialog = $FileDialog
+
 ## The recipe book data
 var recipe_book_data : RecipeBookData = RecipeBookManager.get_recipe_book_data()
 
@@ -20,3 +22,17 @@ func _ready() -> void:
 
 func _on_add_recipe_button_pressed() -> void:
 	SceneManager.load_edit_recipe()
+
+
+func _on_import_recipe_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_export_recipe_button_pressed() -> void:
+	file_dialog.set_current_dir(OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS))
+	file_dialog.set_current_file("recipe_export.json")
+	file_dialog.popup_centered_ratio(1)
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	RecipeImporterExporter.export_to_json(recipe_book_data, path)
+
--- scripts/utility/recipe_importer.gd
+++ scripts/utility/recipe_importer.gd
@@ -2,19 +2,21 @@ extends Node
 class_name RecipeImporterExporter
 
 @export var recipe_book_data : RecipeBookData
-const FILENAME = "res://recipe_book/recipes/recipes_test.json"
+const FILENAME = "user://recipes.json"
 
 
-#func _ready() -> void:
-#	import_from_json()
-
-static func export_to_json(recipe_book_data : RecipeBookData) -> void:
+static func export_to_json(recipe_book_data : RecipeBookData, save_path : String = FILENAME) -> void:
 	var json_result : String = JSON.stringify(RecipeBookManager.data_to_dictionary(), "\t", false)
-	var file = FileAccess.open(FILENAME, FileAccess.WRITE)
+	var file = FileAccess.open(save_path, FileAccess.WRITE_READ)
 	file.store_string(json_result)
 
 static func import_from_json() -> RecipeBookData:
 	var json := JSON.new()
+	if not FileAccess.file_exists(FILENAME):
+		return RecipeBookData.new()
 	var text := FileAccess.open(FILENAME, FileAccess.READ).get_as_text()
 	var parse_result := json.parse(text)
-	return RecipeBookManager.dictionary_to_data(json.data)
+	if parse_result == Error.OK:
+		return RecipeBookManager.dictionary_to_data(json.data)
+
+	return RecipeBookData.new()
